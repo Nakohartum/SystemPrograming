2c4587b20f120a52e6e796aa5bf0a2c9ddaf364f
Nakohartum <artemofgta@gmail.com>
2023-02-23 14:27:53 +0500

    Все три задания сделаны

---
+++ Assets/_Root/Code/TaskExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace _Root.Code
+{
+    public class TaskExample
+    {
+        public static async Task<bool> WhatTaskFasterAsync(CancellationToken ct, Task task1, Task task2)
+        {
+            var any = await Task.WhenAny(task1, task2);
+            return await Task.FromResult(any == task1 && !ct.IsCancellationRequested);
+        }
+    }
+}
